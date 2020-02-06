4d518454f3ca35ccbebd583268a0426ee4f20c68
RichardLiao <bnmghjtyu0@gmail.com>
2020-02-06 17:03:36 +0800

    v1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Home from './pages/Home'
+import './App.css';
+
+const App = () => {
+  document.title = '口罩哩麥造'
+  return (
+    <Home />
+  )
+}
+export default App
