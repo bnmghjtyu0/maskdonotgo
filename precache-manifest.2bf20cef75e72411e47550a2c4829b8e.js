3ce5e24a9e915cbe05983317d09ac7e1d82717bc
Richard.Liao <richard.liao@tpisoftware.com>
2022-04-27 22:58:40 +0800

    Updates

---
+++ precache-manifest.2bf20cef75e72411e47550a2c4829b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97527fcc3c2c54753104ff257f1549df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4cf31baefac762ba3f73",
+    "url": "./static/css/main.6be1d32c.chunk.css"
+  },
+  {
+    "revision": "2010e18a3d6b2038275c",
+    "url": "./static/js/2.b7cbfc3d.chunk.js"
+  },
+  {
+    "revision": "fbaebe9bff0065ae12a925ac6c1a677a",
+    "url": "./static/js/2.b7cbfc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf31baefac762ba3f73",
+    "url": "./static/js/main.591dea54.chunk.js"
+  },
+  {
+    "revision": "b07abf0adbfbca1819d7",
+    "url": "./static/js/runtime-main.19897c70.js"
+  }
+]);
